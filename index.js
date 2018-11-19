438407702eec2285e6ed0a15845f7debdfcab2ac
Drewisatlas <drewisatlas@gmail.com>
2018-11-19 13:20:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file
+function findMatching(drivers, string){
+  return drivers.filter(function (driver) {return driver.toLowerCase() === string.toLowerCase();});
+}
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(function (driver) {return driver.slice(0,2) === string;});
+}
+
+function matchName(drivers, string){
+  return drivers.filter(function (driver) {
+    if (driver.name === string) {
+      return driver;
+    }
+  });
+}
