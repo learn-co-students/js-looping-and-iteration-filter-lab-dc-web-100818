0341b022c2f4a1330d733960c5b1eb71d74e42b3
Jason Campbell <jlcampbell86@gmail.com>
2018-11-18 22:10:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(arr, str) {
+  return arr.filter( function(driver) {
+    return driver.toLowerCase() == str.toLowerCase()
+  })
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter(function(driver) {
+    return driver.startsWith(str)
+  })
+}
+
+function matchName(arr, str) {
+  return arr.filter(function(driver) {
+    return driver.name === str
+  })
+}
